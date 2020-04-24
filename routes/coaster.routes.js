ea560effb59f6eaea31bc561edc1d5f21e01356f
Hector Anton <hectoranf@gmail.com>
2020-04-24 10:21:41 +0200

    done

--- routes/coaster.routes.js
+++ routes/coaster.routes.js
@@ -26,14 +26,13 @@ router.get('/', (req, res, next) => {
 })
 
 router.get('/edit', (req, res, next) => {
-    Coaster.findById(req.query.id)
-        .populate('park')
-        .then(foundCoaster => {
-            Park.find()
-                .then(allParks => res.render('coasters/edit-coaster', { coaster: foundCoaster, parks: allParks }))
-                .catch(error => next(error))
-        })
-        .catch(error => next(error))
+
+    const coasterPromise = Coaster.findById(req.query.id).populate('park')
+    const parkPromise = Park.find()
+
+    Promise.all([coasterPromise, parkPromise])
+        .then(results => res.render('coasters/edit-coaster', { coaster: results[0], parks: results[1] }))
+        .catch(error => next(new Error(error)))
 })
 
 router.post('/edit', (req, res, next) => {
@@ -44,19 +43,17 @@ router.post('/edit', (req, res, next) => {
         .catch(error => next(error))
 })
 
-router.get('/:id', (req, res, next) => {
-    Coaster.findById(req.params.id)
-        .populate('park')
-        .then(foundCoaster => res.render('coasters/coaster-details', foundCoaster))
-        .catch(error => next(error))
-})
-
 router.post('/delete', (req, res, next) => {
     Coaster.findByIdAndRemove(req.query.id)
         .then(res.redirect('/coasters/'))
         .catch(error => next(error))
 })
 
-
+router.get('/:id', (req, res, next) => {
+    Coaster.findById(req.params.id)
+        .populate('park')
+        .then(foundCoaster => res.render('coasters/coaster-details', foundCoaster))
+        .catch(error => next(error))
+})
 
 module.exports = router
